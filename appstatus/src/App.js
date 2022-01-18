8d1efd04ab980fb9c6b57495695abb4cb9553dab
Connor Sullivan <crsullivan@protonmail.com>
2022-01-18 15:02:23 -0700

    Adds state management

--- appstatus/src/App.js
+++ appstatus/src/App.js
@@ -1,27 +1,39 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
 
-const url = 'https://www.salesforce.com/'
+  const [salesforceStatus, setsalesforceStatus] = useState()
+  const [tciStatus, settciStatus] = useState()
+  const [boxStatus, setboxStatus] = useState([])
+  const [schwabStatus, setschwabStatus] = useState([])
+  const [officeStatus, setofficeStatus] = useState()
+  const [tdaStatus, settdaStatus] = useState()
+  const [lastpassStatus, setlastpassStatus] = useState()
+  const [mitelStatus, setmitelStatus] = useState([])
+  const [tamaracStatus, settamaracStatus] = useState([])
+  const [zoomStatus, setzoomStatus] = useState()
 
-// function checkServer(url, timeout) {
-//   const controller = new AbortController();
-//   const signal = controller.signal;
-//   const options = { mode: 'no-cors', signal };
-//   return fetch(url, options)
-//     // .then(setTimeout(() => { controller.abort() }, timeout))
-//     .then(response => console.log('Check server response:', response.status))
-//     .catch(error => console.error('Check server error:', error.message));
-// }
+  const proxy = 'https://criticalappcorsproxy.herokuapp.com/'
 
-// checkServer()
+  async function getSalesforceStatus() {
+    const result = await axios
+    .get(proxy + "https://www.salesforce.com/")
+    .then(res => {
+      console.log(res.status, res.statusText)
+      const salesforceStatus = res.status + ' ' + res.statusText
+      setsalesforceStatus(salesforceStatus)
+    })
+    .catch(error => {
+        console.log(error)
+        alert(error)
+    })
+  }
+useEffect(() => {
+  getSalesforceStatus();
+  }, []);
 
-const controller = new AbortController();
-const signal = controller.signal;
-
-fetch('https://www.salesforceafe.com/' , {mode: 'no-cors', signal})
-.then(response => console.log(response.status))
 
   return (
     <div className="App">
@@ -30,6 +42,7 @@ fetch('https://www.salesforceafe.com/' , {mode: 'no-cors', signal})
           <h2 className='title'>
             Salesforce
           </h2>
+          <p>{salesforceStatus}</p>
         </div>
         <div className='status'>
           <h2 className='title'>
