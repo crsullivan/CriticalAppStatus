e3393814647950fc8634cf4a0b88c12835a8b2ea
Connor Sullivan <connor.sullivan@tciwealth.com>
2022-01-19 09:06:36 -0700

    ready to pull main

--- appstatus/src/App.js
+++ appstatus/src/App.js
@@ -1,27 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
-
-const url = 'https://www.salesforce.com/'
-
-// function checkServer(url, timeout) {
-//   const controller = new AbortController();
-//   const signal = controller.signal;
-//   const options = { mode: 'no-cors', signal };
-//   return fetch(url, options)
-//     // .then(setTimeout(() => { controller.abort() }, timeout))
-//     .then(response => console.log('Check server response:', response.status))
-//     .catch(error => console.error('Check server error:', error.message));
-// }
-
-// checkServer()
-
-const controller = new AbortController();
-const signal = controller.signal;
-
-fetch('https://www.salesforceafe.com/' , {mode: 'no-cors', signal})
-.then(response => console.log(response.status))
 
   return (
     <div className="App">
